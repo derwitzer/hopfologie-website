6bac72145ff0c3ae429e0fe8d0c20cc0b304fcf2
Thomas Witzer <thomas@tweezer.at>
2022-03-13 22:40:43 +0100

    Routing eingebaut

---
+++ assets/js/hopfologie.js
@@ -0,0 +1,29 @@
+function loadContent(routing) {
+    loadFile = null
+    switch (routing) {
+        case "home":
+            loadFile = "assets/html/home.html";
+            break;
+        case "podcast":
+            loadFile = "assets/html/podcast.html";
+            break;
+        case "about":
+            loadFile = "assets/html/ueber_uns.html";
+            break;
+        case "contact":
+            loadFile = "assets/html/kontakt.html";
+            break;
+        case "impressum":
+            loadFile = "assets/html/impressum.html";
+            break;
+        case "privacy":
+            loadFile = "assets/html/datenschutz.html";
+            break;
+        default:
+            loadFile = "assets/html/home.html";
+            break;
+    }
+    $.get(loadFile, function(data){
+        $('#content').html(data);
+    })
+}
